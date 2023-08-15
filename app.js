0059dac4bf2389916cbac151e7f66fa8dbde511c
Guneshwor <117264186+Guneshwor@users.noreply.github.com>
2023-08-16 01:08:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://kingpin886722:atlas0987^@cluster0.5ozclgj.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true},{useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://Gunesh:Gune@123@cluster0.li6blao.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true},{ useUnifiedTopology: true });
 
 const itemsSchema = {
   name: String
